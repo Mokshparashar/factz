c6434bf17327ffc0a1fb0813a0c77c039ce86df9
Moksh parashar <parasharm239@gmail.com>
2024-02-21 07:22:40 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "https://jsbackend-k2xi.onrender.com",
-    },
-    port: 5173,
-  },
+ 
   plugins: [react()],
 });
